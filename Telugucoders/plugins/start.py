ec764b40ccbee82d46189c6a29d6b841149387c4
Wa <122730917+STKR2@users.noreply.github.com>
2023-04-26 14:08:50 +0300

    Update start.py

--- Telugucoders/plugins/start.py
+++ Telugucoders/plugins/start.py
@@ -75,7 +75,7 @@ async def alive(client: Client, message: Message, _):
            ) 
     await message.reply_text(_["alive_start"].format(user_mention),reply_markup=alive_keyboard)
 
-@Client.on_message(command(["السورس", "سورس"]) & ~filters.group & ~filters.edited & ~BANNED_USERS)
+@Client.on_message(command(["السورس", "سورس"]) & filters.group & ~filters.edited)
 @language
 async def repo(client: Client, message: Message, _):
     source_keyboard = InlineKeyboardMarkup( [[
