fdbd6a7f76e2112b1904d0bc412be950393d0618
Wa <122730917+STKR2@users.noreply.github.com>
2023-04-26 12:58:32 +0300

    Update speedtest.py

--- Telugucoders/plugins/speedtest.py
+++ Telugucoders/plugins/speedtest.py
@@ -11,36 +11,35 @@ def testspeed(m):
     try:
         test = speedtest.Speedtest()
         test.get_best_server()
-        m = m.edit("ʀᴜɴɴɪɴɢ ᴅᴏᴡɴʟᴏᴀᴅ sᴘᴇᴇᴅᴛᴇsᴛ")
+        m = m.edit("- تحميل الملفات")
         test.download()
-        m = m.edit("ʀᴜɴɴɪɴɢ ᴜᴘʟᴏᴀᴅ sᴘᴇᴇᴅᴛᴇsᴛ")
+        m = m.edit("- رفع الملفات")
         test.upload()
         test.results.share()
         result = test.results.dict()
-        m = m.edit("sʜᴀʀɪɴɢ sᴘᴇᴇᴅᴛᴇsᴛ ʀᴇsᴜʟᴛs")
+        m = m.edit("انتضر قليلاً ...")
     except Exception as e:
         return m.edit(e)
     return result
 
 
-@app.on_message(command(["speedtest", f"speedtest@{bname}"]) & ~filters.edited)
+@app.on_message(command(["السرعة", f"speedtest@{bname}"]) & ~other_filters & ~filters.edited)
 @sudo_users_only
 async def speedtest_function(client, message):
-    m = await message.reply_text("ʀᴜɴɴɪɴɢ sᴘᴇᴇᴅ ᴛᴇsᴛ")
+    m = await message.reply_text("- تجربة الملفات")
     loop = asyncio.get_event_loop()
     result = await loop.run_in_executor(None, testspeed, m)
-    output = f"""**sᴘᴇᴇᴅᴛᴇsᴛ ʀᴇsᴜʟᴛs**
+    output = f"""**- النتيجة النهائية**
     
-<u>**Client:**</u>
-**__ISP:__** {result['client']['isp']}
-**__Country:__** {result['client']['country']}
-  
-<u>**Server:**</u>
-**__Name:__** {result['server']['name']}
-**__Country:__** {result['server']['country']}, {result['server']['cc']}
-**__Sponsor:__** {result['server']['sponsor']}
-**__Latency:__** {result['server']['latency']}  
-**__Ping:__** {result['ping']}"""
+<u>**- العميل:**</u>
+**__- الانترنت:__** {result['client']['isp']}
+**__- الدولة:__** {result['client']['country']}  
+<u>**- السيرفر:**</u>
+**__- الإسم:__** {result['server']['name']}
+**__- الدولة:__** {result['server']['country']}, {result['server']['cc']}
+**__- الراعي:__** {result['server']['sponsor']}
+**__- الاستجابة:__** {result['server']['latency']}  
+**__- البنك:__** {result['ping']}"""
     msg = await app.send_photo(
         chat_id=message.chat.id, 
         photo=result["share"], 
